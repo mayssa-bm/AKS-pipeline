db5a330b3eaeafc53b43061f087c5c5a173b5f8a
mayssa <60172570+mayssa-bm@users.noreply.github.com>
2021-09-17 15:51:09 +0100

    Update main.tf

--- terraform/AKS+ACR/main.tf
+++ terraform/AKS+ACR/main.tf
@@ -40,6 +40,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     load_balancer_sku = "Standard"
-    network_plugin    = "kubenet" # CNI
+    network_plugin    = "kubenet" 
   }
 }
